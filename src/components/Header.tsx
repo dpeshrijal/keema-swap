61f1bd935a430116e1a2060d3962b9121373d6e6
Deepesh Rijal <deepeshrijal@gmail.com>
2023-03-12 21:37:01 +0545

    Connect wallet button is functional

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,60 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
+declare const window: any;
 
 const Header: FC = () => {
-  return <div className="">Connect</div>;
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [connectText, setConnectText] = useState("Connect");
+
+  const isWalletConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length > 0) {
+        const account = accounts[0];
+
+        setCurrentAccount(account);
+        setConnectText(account.slice(0, 6) + "..." + account.slice(-4));
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
+
+  const handleConnect = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("please install metamask");
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = accounts[0];
+      setCurrentAccount(account);
+      setConnectText(account.slice(0, 6) + "..." + account.slice(-4));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    isWalletConnected();
+  }, []);
+
+  return (
+    <div className="flex justify-between px-8 mt-4">
+      <div className="font-bold text-xl">Keema Swap</div>
+      <button
+        className="text-xl border rounded-full px-4"
+        onClick={handleConnect}
+      >
+        {connectText}
+      </button>
+    </div>
+  );
 };
 
 export default Header;
