6d6f1b3215e5429d9c1ce7fe8db78ff446843639
Deepesh Rijal <deepeshrijal@gmail.com>
2023-03-13 10:03:15 +0545

    disabled mobile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
 import Header from "@/components/Header";
 import Container from "@/components/Container";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { isMobile } from "react-device-detect";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const [currentAccount, setCurrentAccount] = useState("");
+  const router = useRouter();
   const handleAccountSelect = async (value: string) => {
     setCurrentAccount(value);
   };
+  useEffect(() => {
+    if (isMobile) {
+      router.push("/mobile/");
+    }
+  });
   return (
     <div className="flex flex-col h-screen bg-pink-50">
       <Header onAccountSelect={handleAccountSelect} />
---
+++ src/pages/mobile/index.tsx
@@ -0,0 +1,9 @@
+const Mobile = () => {
+  return (
+    <div className="bg-pink-50 h-screen flex justify-center items-center text-xl">
+      <div>Available only on Desktop</div>
+    </div>
+  );
+};
+
+export default Mobile;
